fc20d2356777eea90f4def2ec3d98aae66401e81
beatyt <beatyt@gmail.com>
2017-10-21 12:44:31 -0400

    make test do better checks

--- src/test/java/storage/StorageServiceImplTest.java
+++ src/test/java/storage/StorageServiceImplTest.java
@@ -11,6 +11,9 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
 public class StorageServiceImplTest {
 
     private StorageServiceImpl storageService = new StorageServiceImpl();
@@ -21,32 +24,36 @@ public class StorageServiceImplTest {
 
     @Test
     public void storeEntry() throws Exception {
+        fail("Not implemented");
     }
 
     @Test
     public void retrieveEntry() throws Exception {
+        fail("Not implemented");
     }
 
     @Test
     public void saveFile() throws Exception {
-        List<String> contents = Arrays.asList("Line 1", "Line 2");
+        List<String> contents = Arrays.asList("Line 1", "Line 2", "Line 2");
         String fileName = "testFile.txt";
         storageService.saveFile(contents, fileName);
 
         File file = new File(System.getProperty("user.dir"), fileName);
+        
         List<String> results = IOUtils.readLines(new FileInputStream(file));
-
-        assert results.containsAll(contents);
+        assertThat("The file should contain the inserted data", results.containsAll(contents));
         Set<String> resultsAsSet = new LinkedHashSet<>(results);
-        assert resultsAsSet.equals(contents);
+        assertThat("The results should not contain duplicates", resultsAsSet.size() == results.size());
     }
 
     @Test
     public void appendLineToFile() throws Exception {
+        fail("Not implemented");
     }
 
     @Test
     public void loadFile() throws Exception {
+        fail("Not implemented");
     }
 
 }
